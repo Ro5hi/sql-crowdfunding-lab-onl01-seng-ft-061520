0fc1b352cd95f9341cfe62704b6c1e012bc72aa8
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-15 16:12:21 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -4,12 +4,12 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-  "SELECT users.name, users.age, SUM(pledges.amount) FROM users JOIN pledges on users.id = 
+  "SELECT projects.title, users.age, SUM(pledges.amount) FROM users JOIN pledges on users.id = 
   pledges.user_id GROUP by users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) 
+  "SELECT users.name, (SUM(pledges.amount) - projects.funding_goal) 
   AS amount_left FROM projects JOIN pledges ON projects.id =
   pledges.project_id GROUP BY projects.title HAVING amount_left >= 0;
   "
